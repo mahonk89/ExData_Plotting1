8e6a4b1d74fd63b8f324ed56b70458a331870621
mahonk89 <mahonk5692@gmail.com>
2023-03-30 14:12:43 -0700

    Plots 1:4, scripts and .png files

---
+++ Plot3.R
@@ -0,0 +1,35 @@
+# The first section is collecting and formatting data to create the data frame
+# The second section is code for creating the plot
+# Section 2
+getwd()
+initialdir<-getwd() # WD differs from Dir where data is
+datadir<-"C:/Users/Kevin/Documents/R/Projects/ExploratoryDataAnalysis"
+setwd(datadir) # set to data directory
+setwd(initialdir) # sets back to home directory
+
+# Assigning column names and class
+colheaders<-c("Date","Time","Global_active_power","Global_reactive_power","Voltage",
+              "Global_intensity","Sub_metering_1","Sub_metering_2","Sub_metering_3")
+colclass<-c("character","character","numeric","numeric","numeric","numeric","numeric","numeric","numeric")
+
+# creates DF for required dates
+hhpc<-read.table("household_power_consumption.txt",nrow=2879,skip=66636,sep=";",header=T,
+                 na.strings = c("?","","NA"),colClasses = colclass,col.names = colheaders)
+
+# adds date&time col, formats to date/time
+hhpc$Date<-as.Date(hhpc$Date,"%d/%m/%Y")
+class(hhpc$Date) #check
+hhpc$DateTime<-paste(hhpc$Date,hhpc$Time)
+class(hhpc$DateTime) #check
+hhpc$DateTime<-as.POSIXct(hhpc$DateTime)
+
+# Section 2
+# Plot 3
+with(hhpc,plot(hhpc$DateTime,hhpc$Sub_metering_1,type = "l"
+               ,ylab="Energy Sub Metering",xlab="",col="black"))
+lines(hhpc$DateTime,hhpc$Sub_metering_2,col="red")
+lines(hhpc$DateTime,hhpc$Sub_metering_3,col="blue")
+legend("topright",lty=rep(1,3),legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),
+       col=c("black","red","blue"))
+dev.copy(png,file="Plot3.png",width=480,height=480)
+dev.off()
---
+++ Plot4.R
@@ -0,0 +1,44 @@
+# The first section is collecting and formatting data to create the data frame
+# The second section is code for creating the plot
+# Section 2
+getwd()
+initialdir<-getwd() # WD differs from Dir where data is
+datadir<-"C:/Users/Kevin/Documents/R/Projects/ExploratoryDataAnalysis"
+setwd(datadir) # set to data directory
+setwd(initialdir) # sets back to home directory
+
+# Assigning column names and class
+colheaders<-c("Date","Time","Global_active_power","Global_reactive_power","Voltage",
+              "Global_intensity","Sub_metering_1","Sub_metering_2","Sub_metering_3")
+colclass<-c("character","character","numeric","numeric","numeric","numeric","numeric","numeric","numeric")
+
+# creates DF for required dates
+hhpc<-read.table("household_power_consumption.txt",nrow=2879,skip=66636,sep=";",header=T,
+                 na.strings = c("?","","NA"),colClasses = colclass,col.names = colheaders)
+
+# adds date&time col, formats to date/time
+hhpc$Date<-as.Date(hhpc$Date,"%d/%m/%Y")
+class(hhpc$Date) #check
+hhpc$DateTime<-paste(hhpc$Date,hhpc$Time)
+class(hhpc$DateTime) #check
+hhpc$DateTime<-as.POSIXct(hhpc$DateTime)
+
+# Section 2
+# Plot 4
+par(mfrow=c(2,2))
+with(hhpc,{
+  plot(hhpc$DateTime,hhpc$Global_active_power,type="l",ylab="Global Active Power",xlab="")
+  plot(hhpc$DateTime,hhpc$Voltage,type="l",xlab="datetime",ylab="Voltage")
+  plot(hhpc$DateTime,hhpc$Sub_metering_1,type="l",col="black",xlab="",ylab="Energy sub metering")
+    lines(hhpc$DateTime,hhpc$Sub_metering_2,col="red")
+    lines(hhpc$DateTime,hhpc$Sub_metering_3,col="blue")
+    legend("topright",lty=rep(1,3),legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),
+           col=c("black","red","blue"),bty = "n")
+  plot(hhpc$DateTime,hhpc$Global_reactive_power,type="l",xlab="datetime")
+})
+dev.copy(png,file="Plot4.png",width=480,height=480)
+dev.off()
+
+# since the plot was generated first on screen, then copied to .png with forced
+# pixels, the legend can be affected by resizing if ratio of the on screen
+# plot is not 1:1
